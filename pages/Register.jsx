e6c7c95d044630c30d9e123ea9b8f56ee0c6700a
13228603250 <1768736041@qq.com>
2022-05-16 09:40:16 +0800

    as forth

---
+++ pages/Register.jsx
@@ -0,0 +1,19 @@
+// Register Bussiness
+// Principal : Yichong Wang
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Register extends Component{
+
+    constructor(prop)
+    {
+        super(prop)
+    }
+
+    render()
+    {
+
+        return <h1></h1>
+    }
+}
