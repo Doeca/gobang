f2725302d57273223247d7f0de293b1aa73e9311
iska <1640209219@qq.com>
2022-05-16 09:24:39 +0800

    Router Template

--- App.jsx
+++ App.jsx
@@ -1,1 +1,17 @@
-﻿
+﻿// Basic Router
+// Principal : Xiaoxue Wang
+import React ,{ component } from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends component{
+    constructor()
+    {
+    }
+
+    render()
+    {
+
+        return <h1></h1>
+    }
+}
